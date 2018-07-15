cbbcfb1cb5febe8f73629a1c81eec4082bf614be
atk <atk0@outlook.com>
2018-07-15 03:25:03 +0300

    Added file upload

---
+++ Memberships/Memberships/Controllers/FileController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memberships.Controllers
+{
+    using System.IO;
+
+    [Authorize(Roles = "admin")]
+    public class FileController : Controller
+    {
+        public ActionResult Index()
+        {
+            var path = this.Server.MapPath("~/Content/Files/");
+            var dir = new DirectoryInfo(path);
+            var files = dir.EnumerateFiles().Select(f => f.Name);
+            return this.View(files);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            var path = Path.Combine(this.Server.MapPath("~/Content/Files/"), file.FileName);
+            var data = new byte[file.ContentLength];
+            file.InputStream.Read(data, 0, file.ContentLength);
+            using (var sw = new FileStream(path, FileMode.Create))
+            {
+                sw.Write(data, 0, data.Length);
+            }
+            return this.RedirectToAction("Index");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,19 +1,9 @@
 # MembershipsApp
 
-Add
-0. from building framework
-1. Grids edits from PS course
-2. API
-3. Multitenant
-
-4. Add sqrs
-5. add clean arch
-
-
 replase "admin" with constant
 
 update admin menu with white
 
-test video player with local files
+test video player with local files. no. it can play youtubes only
 
 remove login with other auth providers

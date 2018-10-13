e341cb4ab95466316bec49cac18ee42d6560b889
Andrii Tkach <atk0@outlook.com>
2018-10-13 10:03:38 +0300

    Tabs..

--- Memberships/Memberships/Entities/Item.cs
+++ Memberships/Memberships/Entities/Item.cs
@@ -7,61 +7,61 @@ using System.Web;
 
 namespace Memberships.Entities
 {
-    using System.ComponentModel;
-    using System.Web.Mvc;
+	using System.ComponentModel;
+	using System.Web.Mvc;
 
-    [Table("Item")]
-    public class Item
-    {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+	[Table("Item")]
+	public class Item
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
 
-        [Required]
-        [MaxLength(255)]
-        public string Title { get; set; }
+		[Required]
+		[MaxLength(255)]
+		public string Title { get; set; }
 
-        [MaxLength(2048)]
-        public string Description { get; set; }
+		[MaxLength(2048)]
+		public string Description { get; set; }
 
-        [MaxLength(1024)]
-        public string Url { get; set; }
+		[MaxLength(1024)]
+		public string Url { get; set; }
 
-        [DisplayName("Image Url")]
-        [MaxLength(1024)]
-        public string ImageUrl { get; set; }
+		[DisplayName("Image Url")]
+		[MaxLength(1024)]
+		public string ImageUrl { get; set; }
 
 		[AllowHtml]
-        public string Html { get; set; }
+		public string Html { get; set; }
 
-        [DisplayName("Wait Days")]
-        [DefaultValue(0)]
-        public int WaitDays { get; set; }
+		[DisplayName("Wait Days")]
+		[DefaultValue(0)]
+		public int WaitDays { get; set; }
 
-        public string HtmlShort 
-        {
-        	get 
-        	{
-        		return this.Html == null || this.Html.Length < 50 ? this.Html : this.Html.Substring(0, 50);
-        	}
-        }
+		public string HtmlShort 
+		{
+			get 
+			{
+				return this.Html == null || this.Html.Length < 50 ? this.Html : this.Html.Substring(0, 50);
+			}
+		}
 
-        public int ProductId { get; set; }
+		public int ProductId { get; set; }
 
-        public int ItemTypeId { get; set; }
+		public int ItemTypeId { get; set; }
 
-        public int SectionId { get; set; }
+		public int SectionId { get; set; }
 
-        public int PartId { get; set; }
+		public int PartId { get; set; }
 
-        public bool IsFree { get; set; }
+		public bool IsFree { get; set; }
 
-        [DisplayName("Item Type")]
-        public ICollection<ItemType> ItemTypes { get; set; }
+		[DisplayName("Item Type")]
+		public ICollection<ItemType> ItemTypes { get; set; }
 
-        [DisplayName("Section")]
-        public ICollection<Section> Sections { get; set; }
+		[DisplayName("Section")]
+		public ICollection<Section> Sections { get; set; }
 
-        [DisplayName("Parts")]
-        public ICollection<Part> Parts { get; set; }
-    }
+		[DisplayName("Parts")]
+		public ICollection<Part> Parts { get; set; }
+	}
 }
